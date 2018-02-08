3f032721b09859ce607dbcb63197f8dd5ccabc6c
mjr5736 <mjr5736@psu.edu>
2018-02-07 20:17:44 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 # MVP
 
 Prerequisites:
-1. Ensure Node is installed.
+
+Ensure Node is installed.
 	To install, go to https://nodejs.org/en/download/ and click on the download link.
 
-2. To install the dependencies listed in the 'package.json' file, open a command prompt and run the command:
-	
+Before running for the first time, open a command prompt and run the command:
 	> npm install
+
+	This will install the dependencies listed in the package.json files
 
 To Run:
 1. Open a command prompt and navigate to the directory the files are in.
 
-2. To launch, run the command:
-	
+2. Run the command:
+	> mongod --dbpath 'full-directory-to/data'
+
+	--or-- 
+
+	> mongo "mongodb+srv://cluster0-hiyas.mongodb.net/test" --username mjr5736
+
+3. To launch, run the command:
 	> npm start
 
-3. Open your browser of choice and navigate to http://localhost:3000 to view the map and tweet locations being plotted.
+4. Open your browser of choice and navigate to http://localhost:3000 to view the map and tweet locations being plotted.
 
-4. For additional information about the markers being plotted, open a console window in the browser. The text and location of incoming tweets are displayed in the console.
+5. For additional information about the markers being plotted, open a console window in the browser. The text and location of incoming tweets are displayed in the console.
 
 	Opening the console: https://kb.mailster.co/how-can-i-open-the-browsers-console/
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-var TwitterPackage = require('twitter');
+var Twit = require('twit');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var uri = "mongodb://mjr5736:seni0rDes!gn@cluster0-shard-00-00-hiyas.mongodb.net:27017,cluster0-shard-00-01-hiyas.mongodb.net:27017,cluster0-shard-00-02-hiyas.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+var watson = require('watson-developer-cloud');
+var natural_language_classifier = watson.natural_language_classifier({
+    username: '0780be29-7459-493a-aaaa-0ce2773d41a4',
+    password: '3Grq4DtGw3aR',
+    version: 'v1'
+});
+
+
+//var MongoClient = require('mongodb').MongoClient;
+//var assert = require('assert');
+//
+//
+//
+//var uri = "mongodb://mjr5736:seni0rDes!gn@cluster0-shard-00-00-hiyas.mongodb.net:27017,cluster0-shard-00-01-hiyas.mongodb.net:27017,cluster0-shard-00-02-hiyas.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+//
+//
+
 
 // resolve path to ui page
 app.get('/', function(req, res) {
@@ -18,59 +32,81 @@ app.get('/', function(req, res) {
 var secret = require("./secret");
 
 // make a new Twitter object
-var Twitter = new TwitterPackage(secret);
+var T = new Twit(secret);
 
-// Call the stream function and pass in 'statuses/filter', our filter object, and our callback
-Twitter.stream('statuses/filter', {track: 'weather, rain, snow, sleet, hail, raining, snowing, windy, tornado, hurricane, ice, icy'}, function(stream) {
+/**
+ * filter public stream by the lat/long bounded box of the US and by the English language
+ */
+var top = 49.3457868;       //north lat
+var left = -124.7844079;    // west long
+var right = -66.9513812;    // east long
+var bottom =  24.7433195;   // south lat
 
-    // when we get tweet data
-    stream.on('data', function(tweet) {
+var us = [ left, bottom, right, top ];
+var stream = T.stream('statuses/filter',  { locations: us, language: 'en' });
+stream.on('tweet', function ( tweet ) {
 
-        // if tweet has coordinates
-        if (tweet.coordinates) {
+     // if tweet has coordinates
+     if (tweet.coordinates) {
 
-            if (tweet.coordinates !== null){
-                var geoLng = tweet.coordinates.coordinates[0];
-                var geoLat = tweet.coordinates.coordinates[1];
-            }
-            else if(tweet.place){
+         if (tweet.coordinates !== null){
+             var geoLng = tweet.coordinates.coordinates[0];
+             var geoLat = tweet.coordinates.coordinates[1];
+         }
+         else if(tweet.place){
 
-                if(tweet.place.bounding_box === 'Polygon'){
-                    // Calculate the center of the bounding box for the tweet
-                    var coord, _i, _len;
-                    var centerLat = 0;
-                    var centerLng = 0;
-                    for (_i = 0, _len = coords.length; _i < _len; _i++) {
-                        coord = coords[_i];
-                        centerLat += coord[0];
-                        centerLng += coord[1];
-                    }
-                    geoLng = centerLat / coords.length;
-                    geoLat = centerLng / coords.length;
-                }
-            }
-            // print out the text of the tweet that came in
-            console.log(tweet.text);
+             if(tweet.place.bounding_box === 'Polygon'){
+                 // Calculate the center of the bounding box for the tweet
+                 var coord, _i, _len;
+                 var centerLat = 0;
+                 var centerLng = 0;
+                 for (_i = 0, _len = coords.length; _i < _len; _i++) {
+                     coord = coords[_i];
+                     centerLat += coord[0];
+                     centerLng += coord[1];
+                 }
+                 geoLng = centerLat / coords.length;
+                 geoLat = centerLng / coords.length;
+             }
+         }
 
-            var geoPoint = {lat: geoLat, lng: geoLng};
-            console.log(geoLat + ' ' + geoLng);
-            console.log();
+         var geoPoint = {lat: geoLat, lng: geoLng};
 
-            // send tweet event to client
-            io.emit('tweetEvent', geoPoint, tweet);
+         /**
+          * check with the natural language classifier to see if the tweet's text is weather-related
+          */
+         //natural_language_classifier.classify({
+         //        text: tweet.text,
+         //        classifier_id: '33c2fbx273-nlc-8618' },
+         //    function(err, response) {
+         //        if (err)
+         //            console.log('error:', err);
+         //        else {
+                     console.log(tweet.text);
+         //            console.log(response.top_class);
+         //            console.log('\n\n')
+//
+         //            if ( response.top_class === 'weather') {
+                         io.emit('tweetEvent', geoPoint, tweet);
+                         console.log();
 
-            // insert tweet in database
-            MongoClient.connect(uri, function(err, client) {
-                if (err) throw err;
-                const collection = client.db("test").collection("MVP");
-                collection.insertOne(tweet, function(err, res) {
-                    if (err) throw err;
-                    console.log('doc inserted!');
-                });
-                client.close();
-            });
-        }
-    });
+         //                /**
+         //                 * connect to database and insert tweet
+         //                 */
+         //                //MongoClient.connect(uri, function(err, client) {//
+         //                //    if (err) throw err;
+         //                //    const collection = client.db("test").collection("MVP");
+         //                //    collection.insertOne(tweet, function(err, res) {
+         //                //        if (err) throw err;
+         //                //        console.log('doc inserted!');
+         //                //    });
+         //                //    client.close();
+         //                //});
+         //            }
+         //        }
+         //    });
+     }
+ });
 
     /* stream functions */
     stream.on('error', function(error) {
@@ -85,7 +121,7 @@ Twitter.stream('statuses/filter', {track: 'weather, rain, snow, sleet, hail, rai
     stream.on('disconnect', function(disconnect) {
         console.log(disconnect);
     });
-});
+
 
 // connect to localhost
 http.listen(3000, function() {
