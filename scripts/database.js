09ed1dc9b0917d71cfac1906ebd764a10a981885
jae0014 <jaedafeeder@gmail.com>
2018-04-26 18:36:20 -0400

    Fixed the any search

--- scripts/database.js
+++ scripts/database.js
@@ -2,6 +2,7 @@
  * connect to database and insert tweet
  */
 let MongoClient = require( 'mongodb' ).MongoClient;
+let moment =require('moment');
 let uri = "mongodb://mjr5736:sen!0rDesign@seniordesign-shard-00-00-hiyas.mongodb.net:27017,seniordesign-shard-00-01-hiyas.mongodb.net:27017,seniordesign-shard-00-02-hiyas.mongodb.net:27017/test?ssl=true&replicaSet=SeniorDesign-shard-0&authSource=admin";
 let ex = module.exports = {};
 
@@ -39,16 +40,12 @@ function getAllTweetsFromDB( sendTweets, socket ) {
 	} );
 }
 
-function getSearchResults( searchVars, sendTweets ) {
-
-printRes( searchVars );
-	let key = keyForDB;
+function getSearchResults( searchVars, sendTweets, socket ) {
+	printRes( searchVars );
+	var key = keyForDB;
 	let city = cityStateForDB
 	let start = sDate;
 	let end = eDate;
-	console.log( start );
-	console.log( end );
-	console.log( key + " Before connect" );
 	MongoClient.connect( uri, function ( err, db ) {
 
 		if ( err ) throw err;
@@ -58,104 +55,38 @@ printRes( searchVars );
 		let collection = database.collection( "test" );
 		if ( city == "" )
 			city = any;
-		if ( key == "" )
+		if ( key == "" || key == "(.**.)" )
 			key = any;
-		if ( start == "" )
-			start = "2010-04-19T11:43:47.875Z";
-		if ( end == "" )
-			end = new Date().toISOString;
+		if(eDate == "")
+			eDate=new Date("2019-05-18T16:00:00Z").toISOString();
+		if(sDate == "")
+			sDate =new Date("2010-05-18T16:00:00Z").toISOString();
+		
+		console.log(eDate+	 " " + sDate+	 " " + city+	 " " + key)
 		collection.find(
 			//{"text":{$regex: "(.*snow*.|rain)"}},
-
 			{
 				$and:
-					[       // search for key word
-						//{"place.full_name":{$regex:any}},
-						//text search regext snow or rain or fire
-						//{"text":{$regex: "(.*snow*.)"}},
-						//{"place.place_type":{$regex:"city"}}
-
-						// find place by its location
-
+					[    
 						{ "place.full_name": { $regex: city } },
-						// text search regext snow or rain or fire
 						{ "text": { $regex: key } },
-						{ "place.place_type": { $regex: "city" } },
-						/*{
-							created_at: {
-								$gte: ISODate(start),
-								$lt: ISODate(end)
-							}
-						}*/
-
+						//{ "place.place_type": { $regex: "" } }
+						{"created_at": {
+							$gte: sDate,
+							$lte: eDate
+						}}
 					]
 
 			}
 		).toArray( function ( err, result ) {
-			//Wconsole.log(result);
-			sendTweets( result );
+			//console.log(result);
+			sendTweets( result, socket );
 			db.close();
 		} );
 	} );
 	keyForDB = "" , cityStateForDB = "", sDate0 = "", eDate;
 
-    printRes( searchVars );
-    console.log(keyForDB);
-    console.log(cityStateForDB);
-    var key = keyForDB;
-    var city = cityStateForDB
-    var start = sDate;
-    var end = eDate;
-    
-    console.log(snow);
-    MongoClient.connect(uri, function (err, db) {
-        
-        if (err) throw err;
-        var any = ".*";
-        //var tempDate = new Date("Sun Apr 1 21:46:15 +0000 2017".toISOString())
-        let database = db.db("test");
-        let collection = database.collection("test");
-        if(cityStateForDB == "")
-        city = any; 
-        if(keyForDB== "")
-        key = any;
-        if(sDate== "")
-        start = "2010-04-19T11:43:47.875Z"; 
-        if(eDate== "")
-        end = new Date().toISOString;  
-        collection.find( 
-            //{"text":{$regex: "(.*snow*.|rain)"}},
-            
-            {$and:
-                [       // search for key word 
-                        //{"place.full_name":{$regex:any}},
-                         //text search regext snow or rain or fire 
-                        //{"text":{$regex: "(.*snow*.)"}},
-                        //{"place.place_type":{$regex:"city"}}
-                         
-                        // find place by its location 
-                        
-                        {"place.full_name":{$regex:city}},
-                        // text search regext snow or rain or fire 
-                        {"text":{$regex: key}},
-                        {"place.place_type":{$regex:"city"}},
-                        {"created_at": {
-                            $gte: ISODate(start),
-                            $lt: ISODate(end)
-                        }}
-                  ]
- 
-            }
-        ).toArray( function( err, result ) {
-           //Wconsole.log(result);
-            sendTweets( result );
-            db.close();
-        });
-    });
-    keyForDB = "" , cityStateForDB ="",sDate0 ="", eDate;
-
 }
-
 function printRes( searchVars ) {
 
 	let keywords = searchVars.keywords,
@@ -163,131 +94,61 @@ function printRes( searchVars ) {
 		states = searchVars.states,
 		startDate = searchVars.startDate,
 		endDate = searchVars.endDate;
-	startDate = sDate;
-	console.log( "Start Date is " + sDate )
-	endDate = eDate;
-	console.log( "End Date is " + sDate )
+	
 	let k = '',
 		c = '',
 		s = '';
+		console.log("date Starts here");
+		console.log(startDate);
+		var sdate = moment(startDate);
+		var edate = moment(endDate);
+		if(sdate.isValid())
+		{
+			sDate = new Date(startDate).toISOString();
 
-	if ( keywords !== undefined ) {
+		}
+		else
+		{
+			sDate = "";
+		}
+		if(edate.isValid())
+		{
+			eDate = new Date(endDate).toISOString();
+
+		}	
+		else
+		{
+			eDate = "";
+		}
+	if ( keywords !== undefined )
+	{
 		k = keywords;
-		keyForDB = splitWord( k, ' ' );
-		console.log( keyForDB + " Key For DB" );
+		keyForDB = splitWord(k , ' ');
 	}
 	else
 		k = 'couldn\'t get keywords';
-
-
-	if ( states !== undefined ) {
+	if ( states !== undefined )
+	{
 		s = states;
-		if ( s.length != 0 )
-			cityStateForDB = splitWord( s, ',' )
+		if(s.length != 0 )
+			cityStateForDB = splitWord(s , ',')
 	}
-	else {
+	else
+	{
 		s = 'couldn\'nt get states';
 	}
-	if ( cities !== undefined ) {
+	if ( cities !== undefined )
+	{
 		c = cities;
-		if ( s.length != 0 )
-			cityStateForDB += "|" + c + ")";
+		if(s.length != 0 )
+			cityStateForDB += "|" + c +")";
 
 	}
-	else {
+	else{
 		c = 'couldn\'t get cities';
-		if ( s.length != 0 )
+		if(s.length != 0 )
 			cityStateForDB += ")";
-	}
-	/*
-		console.log( '\n\n' +
-			k.toString() + '\n' +
-			c.toString() + '\n' +
-			s.toString() + '\n' +
-			startDate.toString() + '\n' +
-			endDate.toString() + '\n\n'
-		);*/
-	return null;
-}
-
-function splitWord( str, splitVal ) {
-	let returnStr = "(";
-
-	//for state
-	if ( splitVal == ',' ) {
-		let temparray = str;
-		temparray.forEach( function ( e, i, array ) {
-			if ( i == 0 ) {
-				let temp = e.toUpperCase();
-				returnStr += e;
-				returnStr += "|" + temp
-			}
-			else {
-				let temp = e.toUpperCase();
-				returnStr += "|" + e;
-				returnStr += "|" + temp
-			}
-		} )
-	}
-	else if ( splitVal == ' ' ) {
-		console.log( 'hi' )
-		let temparray = str.split( splitVal );
-		temparray.forEach( function ( e, i, array ) {
-			if ( i == 0 ) {
-				returnStr += ".*" + e;
-			}
-			else {
-				returnStr += "|.*" + e;
-			}
-		} )
-		returnStr += ")"
-	}
-	return returnStr;
-
-    let keywords = searchVars.keywords,
-        cities = searchVars.cities,
-        states = searchVars.states,
-        startDate = searchVars.startDate,
-        endDate = searchVars.endDate;
-    startDate = sDate;
-    endDate= eDate;
-    let k = '',
-        c = '',
-        s = '';
-    
-    if ( keywords !== undefined )
-    {
-        k = keywords;
-        keyForDB = splitWord(k , ' ');
-        console.log(keyForDB+ " Key For DB");
-    }
-    else
-        k = 'couldn\'t get keywords';
-
-   
-
-    if ( states !== undefined )
-    {
-        s = states;
-        if(s.length != 0 )
-        cityStateForDB = splitWord(s , ',')
-    }
-    else
-    {   
-        s = 'couldn\'nt get states';
-    }
-    if ( cities !== undefined )
-    {
-        c = cities;
-        if(s.length != 0 )
-        cityStateForDB += "|" + c +")"; 
-        
-    }
-    else{
-        c = 'couldn\'t get cities';
-        if(s.length != 0 )
-        cityStateForDB += ")";
-    }/*
+	}/*
     console.log( '\n\n' +
         k.toString() + '\n' +
         c.toString() + '\n' +
@@ -295,44 +156,44 @@ function splitWord( str, splitVal ) {
         startDate.toString() + '\n' +
         endDate.toString() + '\n\n'
     );*/
-    return null; 
+	return null;
 }
 function splitWord(str, splitVal)
 {   var returnStr = "(";
-    
-    //for state 
-    if(splitVal == ',')
-    {var temparray = str;
-        temparray.forEach(function(e,i,array)
-        {  if(i == 0)
-            {   var temp = e.toUpperCase();
-                returnStr += e;
-                returnStr += "|" +temp
-            }
-            else
-            {   var temp = e.toUpperCase();
-                returnStr += "|"+e;
-                returnStr += "|" +temp
-            }
-        })
-    }
-    else if (splitVal == ' ')
-    {
-        console.log('hi')
-        var temparray = str.split(splitVal);
-    temparray.forEach(function(e,i,array)
-    {  if(i == 0)
-        {
-            returnStr += ".*"+e+"*.";
-        }
-        else
-        {
-            returnStr += "|.*"+e+"*.";
-        }
-         })
-         returnStr+= ")"
-    }
-    return returnStr;
+
+	//for state
+	if(splitVal == ',')
+	{var temparray = str;
+		temparray.forEach(function(e,i,array)
+		{  if(i == 0)
+		{   var temp = e.toUpperCase();
+			returnStr += e ;
+			returnStr += "|" +temp
+		}
+		else
+		{   var temp = e.toUpperCase();
+			returnStr += "|"+e;
+			returnStr += "|" +temp
+		}
+		})
+	}
+	else if (splitVal == ' ')
+	{
+
+		var temparray = str;
+		temparray.forEach(function(e,i,array)
+		{  if(i == 0)
+		{
+			returnStr += ".*"+e+"*.";
+		}
+		else
+		{
+			returnStr += "|.*"+e+"*.";
+		}
+		})
+		returnStr+= ")"
+	}
+	return returnStr;
 }
 
 ex.addTweetToDB = addTweetToDB;
